a6124539c1553945afa04866bf35685f7b19bf53
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-26 10:32:54 -0800

    textfiled, and Dropdown together

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,111 @@
-import 'package:flutter/foundation.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 void main(){
   runApp(const MaterialApp(
-    title: "flutter app",
+    title: " Simple Interest Calculator App",
     home: homepage(),
   ));
 }
+
 class homepage extends StatefulWidget {
   const homepage({Key? key}) : super(key: key);
+
   @override
   _homepageState createState() => _homepageState();
 }
 
 class _homepageState extends State<homepage> {
-  String name="";
- String changeditems="girma";
-
+  List<String>namelist=["dollar",'yeroo','rupii'];
+  final _miniPadding=5.0;
+  String namevalue="name";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("flutter  app"),
-        centerTitle: true,
+        title: const Text("Calculator app"),
+        centerTitle:true ,
       ),
       body: Container(
         child: Column(
           children: <Widget>[
-            TextField(
-              onSubmitted: (String userinput){
-                setState(() {
-                  name= userinput;
+            getimage(),
+          Padding(
+            padding:  EdgeInsets.all(_miniPadding),
+            child: TextField(
+             keyboardType: TextInputType.number,
+             decoration: InputDecoration(
+               labelText: "enter your name",
+               hintText: 'name',
+               border: OutlineInputBorder(
+                 borderRadius: BorderRadius.circular(8.0),
 
-                });
 
-              },
-            ),
+               )
+
+             ),
+         ),
+          ),
             Padding(
-              padding: const EdgeInsets.all(28.0),
-              child: Text("current user input is $name"),
+              padding:  EdgeInsets.all(_miniPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "enter your name",
+                    hintText: 'name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+
+
+                    )
+
+                ),
+              ),
             ),
-             DropdownButton<String>(
-                 items: <String>["solomon","girma","Teshome"].map((String index){
-                   return DropdownMenuItem<String>(
-                       child: Text(index),
-                     value: index,
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        hintText: "eneter number",
+                        labelText: 'please eneter name',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                        )
+                    ),
+                  ),
+                ),
+                DropdownButton(
+                  items: <String>['name','city','adress'].map((e){
+                    return DropdownMenuItem<String>(
+                      value: e,
+                      child: Text(e),
+                    );
+                  }).toList(),
+                  onChanged: (index){
+                    setState(() {
+                      namevalue=index as String;
+                    });
 
-                   );
-                 }).toList(),
-            onChanged: (indexitem){
-           setState(() {
-             changeditems=indexitem!;
-           });
-
-            },
-               value: changeditems,
-             )
-
-
+                  },
+                  value: namevalue,
+                )
+              ],
+            )
           ],
         ),
       ),
-
     );
   }
+
+  Widget getimage() {
+    AssetImage assetImage= AssetImage("images/dollar.PNG");
+    Image image= Image(image: assetImage,width: 325.0,height: 225.0,);
+    return Container(
+      child:image,
+      margin: EdgeInsets.all(_miniPadding*0.1),
+    );
+
+  }
 }
+
