45c343795b350b95b50ce3b35bd02516ac8e617b
Solomon Girma <92579234+like-github1@users.noreply.github.com>
2021-11-27 04:39:51 -0800

    Simple_calculate_application
    Simple_calculate_application using Flutter Frameworks

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     title: " Simple Interest Calculator App",
     home: homepage(),
     //flutter primerycolor
-
   ));
 }
 
@@ -18,160 +18,177 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
-  List<String>namelist=["dollar",'yeroo','rupii'];
-  final _miniPadding=5.0;
-  String namevalue="dollar";
-  TextEditingController textEditingController=TextEditingController();
-  TextEditingController principaltextEditingControllers=TextEditingController();
-  TextEditingController termtextEditingController=TextEditingController();
- double Dispalytext= 0.0;
+  List<String> namelist = ["dollar", 'yeroo', 'rupii'];
+  final _miniPadding = 5.0;
+  String namevalue = "dollar";
+  //it is a uniqe key for our FormState
+  final _formkey=GlobalKey<FormState>();
+  TextEditingController textEditingController = TextEditingController();
+  TextEditingController principaltextEditingControllers =
+      TextEditingController();
+  TextEditingController termtextEditingController = TextEditingController();
+  double Dispalytext = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // resizeToAvoidBottomInset: false,//avoids bottom overflow
+      // resizeToAvoidBottomInset: false,//avoids bottom overflow
       appBar: AppBar(
         title: const Text("Calculator app"),
-        centerTitle:true ,
+        centerTitle: true,
       ),
-      body: Container(
+      body: Form(
+        key: _formkey,
+          child: Padding(
+        padding: EdgeInsets.all(8),
         child: ListView(
           children: <Widget>[
             getimage(),
-          Padding(
-            padding:  EdgeInsets.all(_miniPadding),
-            child: TextField(
-              controller: textEditingController,
-             keyboardType: TextInputType.number,
-             decoration: InputDecoration(
-               labelText: "enter your name",
-               hintText: 'name',
-               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(8.0),
-
-
-               )
-
-             ),
-         ),
-          ),
             Padding(
-              padding:  EdgeInsets.all(_miniPadding),
-              child: TextField(
-                controller: principaltextEditingControllers,
+              padding: EdgeInsets.all(_miniPadding),
+              child: TextFormField(
+                controller: textEditingController,
+                validator: (String? value){
+                  if(value==null|| value.isEmpty){
+                    return "please enter a valid input";
+                  }},
                 keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "enter your name",
+                    hintText: 'name',
+                    errorStyle: const TextStyle(
+                      color: Colors.red,
+                      fontSize: 15,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(_miniPadding),
+              child: TextFormField(
+                controller: principaltextEditingControllers,
+                validator: (String ?value){
+                  if(value==null|| value.isEmpty){
+                    return "you have invalid input";
+                  }
+                  },
+                keyboardType: TextInputType.number,
+
                 decoration: InputDecoration(
                     labelText: "principal",
                     hintText: 'enter principal',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(8.0),
-
-
-                    )
-
-                ),
+                    )),
               ),
             ),
             Row(
               children: <Widget>[
                 Expanded(
-                  child: TextField(
+                  child: TextFormField(
+                    validator: (String ?value){
+                      if(value==null|| value.isEmpty){
+                        return "you have invalid input";
+                      }
+                    },
                     controller: termtextEditingController,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
+                      errorStyle: TextStyle(
+                        color: Colors.red,
+                      ),
                         hintText: "eneter number",
                         labelText: 'Term',
                         border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(5.0),
-                        )
-                    ),
+                          borderRadius: BorderRadius.circular(5.0),
+                        )),
                   ),
                 ),
-
-               Expanded(
-                    child: DropdownButton(
-                      items: namelist.map((e){
-                        return DropdownMenuItem<String>(
-                          value: e,
-                          child: Text(e),
-                        );
-                      }).toList(),
-                      onChanged: (index){
-                        doropdownSelecteditem(index);
-                      },
-                      value: namevalue,
-                    ),
-
+                Expanded(
+                  child: DropdownButton(
+                    items: namelist.map((e) {
+                      return DropdownMenuItem<String>(
+                        value: e,
+                        child: Text(e),
+                      );
+                    }).toList(),
+                    onChanged: (index) {
+                      doropdownSelecteditem(index);
+                    },
+                    value: namevalue,
+                  ),
                 ),
-
               ],
             ),
             Row(
               children: <Widget>[
-                 Expanded(
-                    child: ElevatedButton(
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if(_formkey.currentState!.validate()){
+                          Dispalytext = getPrincipal();
 
-                      onPressed: (){
-                        setState(() {
-                          Dispalytext=getPrincipal();
+                        }
 
-                        });
-                      },
-                      child: Text("calculet"),
-                    ),
+                      });
+                    },
+                    child: Text("calculet"),
                   ),
-                 Expanded(
-                    child: ElevatedButton(
-                      onPressed: (){
-                    setState(() {
-                      _reset();
-                    });
-                      },
-                      child: Text("reset"),
-
-                    ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _reset();
+                      });
+                    },
+                    child: Text("reset"),
                   ),
-
+                ),
               ],
             ),
             Text("your Current principal is $Dispalytext")
           ],
         ),
-      ),
+      )),
     );
   }
 
   Widget getimage() {
-    AssetImage assetImage= AssetImage("images/dollar.PNG");
-    Image image= Image(image: assetImage,width: 325.0,height: 225.0,);
-    return Container(
-      child:image,
-      margin: EdgeInsets.all(_miniPadding*0.1),
+    AssetImage assetImage = AssetImage("images/dollar.PNG");
+    Image image = Image(
+      image: assetImage,
+      width: 325.0,
+      height: 225.0,
     );
-
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_miniPadding * 0.1),
+    );
   }
 
   void doropdownSelecteditem(Object? index) {
     setState(() {
-      namevalue=index as String;
+      namevalue = index as String;
     });
   }
 
   double getPrincipal() {
-    double principal= double.parse(principaltextEditingControllers.text);
-    double text= double.parse(textEditingController.text);
-    double term= double.parse(this.termtextEditingController.text);
-    double totalamountopay= principal+(principal*text*term)/100;
+    double principal = double.parse(principaltextEditingControllers.text);
+    double text = double.parse(textEditingController.text);
+    double term = double.parse(this.termtextEditingController.text);
+    double totalamountopay = principal + (principal * text * term) / 100;
     return totalamountopay;
-
   }
 
   void _reset() {
-     Dispalytext=0.0;
-    termtextEditingController.text="";
-     principaltextEditingControllers.text="";
-   textEditingController.text="";
-     namevalue=namelist[0];
+    Dispalytext = 0.0;
+    termtextEditingController.text = "";
+    principaltextEditingControllers.text = "";
+    textEditingController.text = "";
+    namevalue = namelist[0];
   }
 }
-
-
